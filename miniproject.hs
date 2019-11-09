8b2da45562ee9b5a13da5cd800a7673a0d80a68b
Richard Tarnoczi <richard@autopi.io>
2019-11-09 16:33:59 +0100

    made a bit more readable
    more readable functions and added already made functions from the first problem set

--- miniproject.hs
+++ miniproject.hs
@@ -5,13 +5,57 @@ frequencies :: [Char] -> [(Char, Int)]
 frequencies [] = []
 frequencies strToCount = sortfrequencies (auxFrequencies strToCount [] [])
 
-        
+--TODO: add where or let to make it shorter        
 auxFrequencies [] checkedChars lstCharOccurrence = lstCharOccurrence
 auxFrequencies strToCount checkedChars lstCharOccurrence =
     if elem (head strToCount) checkedChars
-        then auxFrequencies (tail strToCount) checkedChars lstCharOccurrence
-        else auxFrequencies (tail strToCount) (checkedChars ++ [(head strToCount)]) (lstCharOccurrence ++ [((head strToCount), (length (filter (==(head strToCount)) strToCount)))])
+        then auxFrequencies restStr checkedChars lstCharOccurrence
+        else auxFrequencies restStr (checkedChars ++ [firstC]) (lstCharOccurrence ++ [(firstC, (length (filter (==firstC) strToCount)))])
+        where restStr = tail strToCount
+              firstC = head strToCount
 
 sortfrequencies :: Ord a => [(a1, a)] -> [(a1, a)]
 sortfrequencies [] = []
 sortfrequencies tuplesToSort = sortBy (flip compare `on` snd) tuplesToSort
+
+
+
+
+
+valid :: (Eq a, Eq a1) => [(a1, a)] -> Bool
+valid [] = False
+valid ((a,b):xs)
+    | elem a (restArgumentList xs) = False
+    | elem b (restValueList xs) = False
+    | tail xs == [] = True
+    | otherwise = valid (head xs : tail xs)
+
+restArgumentList :: [(b, b1)] -> [b]
+restArgumentList list = map fst list
+
+restValueList :: [(a, b)] -> [b]
+restValueList list = map snd list
+
+--findfun :: [(a, b)] -> f
+
+--although there exist another function called "lookup" which kinda does the same thing
+lookup' :: Eq a => [(a, [Char])] -> a -> [Char]
+lookup [] a = "Value does not exist."
+lookup' l a = 
+    if valid l
+        then lookupAux l a
+        else "Value does not exsist."
+
+lookupAux :: Eq a => [(a, [Char])] -> a -> [Char]      
+lookupAux l a =
+    if fst (head l) == a
+        then snd (head l)
+        else if (length (tail l)) > 0
+            then lookupAux (tail l) a
+            else "Value does not exist."
+
+findfun :: (t, [Char]) -> [Char] -> [Char]
+findfun (a,b) x = 
+    if b == x
+        then b
+        else "Function not found."
