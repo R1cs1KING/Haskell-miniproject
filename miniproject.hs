9b7e5246eaab83f994aca8a4426d1370ee9b2576
Richard Tarnoczi <richard@autopi.io>
2019-11-12 23:55:37 +0100

    working prototype

--- miniproject.hs
+++ miniproject.hs
@@ -16,9 +16,20 @@ getSymbol :: LTree -> [Symbol]
 getSymbol (Leaf s _) = [s]
 getSymbol (Branch _ _ symbols _) = symbols
 
-frequencies :: [Char] -> LTree
---frequencies [] = []
-frequencies strToCount = buildHuffmanTree (listToLTree (reverseFrequencies (sortfrequencies (auxFrequencies strToCount [] []))))
+encodingString str = auxEncodingString str (start str) []
+
+auxEncodingString [] tree encodedStr = encodedStr
+auxEncodingString (x:xs) tree encodedStr = 
+    if length xs == 0
+        then (encodedStr ++ (encodeChar x tree []))
+        else auxEncodingString xs tree (encodedStr ++ (encodeChar x tree []))
+
+start :: [Char] -> LTree
+start str = buildHuffmanTree (sortTreeList (listToLTree (sortfrequencies (frequencies str))))
+
+frequencies :: Eq a => [a] -> [(a, Int)]
+frequencies [] = []
+frequencies strToCount = auxFrequencies strToCount [] []
 
 --TODO: add where or let to make it shorter        
 auxFrequencies [] checkedChars lstCharOccurrence = lstCharOccurrence
@@ -31,14 +42,15 @@ auxFrequencies strToCount checkedChars lstCharOccurrence =
 
 sortfrequencies :: Ord a => [(a1, a)] -> [(a1, a)]
 sortfrequencies [] = []
-sortfrequencies tuplesToSort = sortBy (flip compare `on` snd) tuplesToSort
+sortfrequencies tuplesToSort = sortBy (compare `on` snd) tuplesToSort
 
 sortTreeList :: [LTree] -> [LTree]
 sortTreeList treeToSort = sortBy (compare `on` getFrequency) treeToSort
 
--- check if the list of pairs is a valid association list (the argument won't repeat) CHECK THE TAIL XS PART
+-- check if the list of pairs is a valid association list (the argument won't repeat)
 valid :: (Eq a, Eq a1) => [(a1, a)] -> Bool
 valid [] = False
+valid [a] = True
 valid ((a,b):xs)
     | elem a (restArgumentList xs) = False
     | tail xs == [] = True
@@ -75,11 +87,6 @@ treemerge lt1 lt2 = Branch lt1 lt2 treeSymbols treeFrequency
         where treeSymbols = ((getSymbol lt1) ++ (getSymbol lt2))
               treeFrequency = ((getFrequency lt1) + (getFrequency lt2))
 
--- function that reverses a list
-reverseFrequencies :: [(Char, Int)] -> [(Char, Int)]
-reverseFrequencies [] = []
-reverseFrequencies (x:xs) = reverseFrequencies xs ++ [x]
-
 -- function to turn a list of tuples into leaves
 listToLTree :: [(Char, Int)] -> [LTree]
 listToLTree [] = []
@@ -95,3 +102,17 @@ auxListToLTree lst lstTree =
 buildHuffmanTree :: [LTree] -> LTree
 buildHuffmanTree [t] = t
 buildHuffmanTree (t1:t2:ts) = buildHuffmanTree (sortTreeList ((treemerge t1 t2):ts))
+
+encodeChar c (Leaf s f) encoding = encoding
+encodeChar c (Branch l r syms f) encoding =
+    if elem c (getSymbol l)
+        then encodeChar c l (encoding ++ "0")
+        else encodeChar c r (encoding ++ "1")
+
+--decodeStr str tree 
+decodeCharacter [] tree (Leaf s f) decodedStr = (decodedStr ++ [s])
+decodeCharacter enc tree (Leaf s f) decodedStr =  decodeCharacter enc tree tree (decodedStr ++ [s])
+decodeCharacter enc tree (Branch l r syms f) decodedStr =
+    if (head enc) == '0'
+        then decodeCharacter (tail enc) tree l decodedStr
+        else decodeCharacter (tail enc) tree r decodedStr
