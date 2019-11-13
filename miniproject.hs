21ab9b9f0a6654031f29cd293f0b2f43c07aef34
Richard Tarnoczi <richard@autopi.io>
2019-11-13 13:35:28 +0100

    cleaned  version

--- miniproject.hs
+++ miniproject.hs
@@ -16,34 +16,46 @@ getSymbol :: LTree -> [Symbol]
 getSymbol (Leaf s _) = [s]
 getSymbol (Branch _ _ symbols _) = symbols
 
-encodingString str = auxEncodingString str (start str) []
+-- function to encode a string
+encodingStr :: [Symbol] -> [Char]
+encodingStr [] = []
+encodingStr str = auxEncodingStr str (startTreeBuild str) []
 
-auxEncodingString [] tree encodedStr = encodedStr
-auxEncodingString (x:xs) tree encodedStr = 
+auxEncodingStr :: [Symbol] -> LTree -> [Char] -> [Char]
+auxEncodingStr [] tree encodedStr = encodedStr
+auxEncodingStr(x:xs) tree encodedStr = 
     if length xs == 0
         then (encodedStr ++ (encodeChar x tree []))
-        else auxEncodingString xs tree (encodedStr ++ (encodeChar x tree []))
+        else auxEncodingStr xs tree (encodedStr ++ (encodeChar x tree []))
 
-start :: [Char] -> LTree
-start str = buildHuffmanTree (sortTreeList (listToLTree (sortfrequencies (frequencies str))))
+-- function that takes a string and applies all functions that are needed to build a tree
+startTreeBuild :: [Char] -> LTree
+startTreeBuild str = buildHuffmanTree (sortTreeList (listToLTree (sortfrequencies (frequencies str))))
 
+-- function that creates a list of tuples from a string,
+-- where the first value is a character and the second is an integer
 frequencies :: Eq a => [a] -> [(a, Int)]
 frequencies [] = []
-frequencies strToCount = auxFrequencies strToCount [] []
+frequencies str = auxFrequencies str [] []
 
 --TODO: add where or let to make it shorter        
-auxFrequencies [] checkedChars lstCharOccurrence = lstCharOccurrence
-auxFrequencies strToCount checkedChars lstCharOccurrence =
-    if elem (head strToCount) checkedChars
-        then auxFrequencies restStr checkedChars lstCharOccurrence
-        else auxFrequencies restStr (checkedChars ++ [firstC]) (lstCharOccurrence ++ [(firstC, (length (filter (==firstC) strToCount)))])
-        where restStr = tail strToCount
-              firstC = head strToCount
+auxFrequencies :: Eq a => [a] -> [a] -> [(a, Int)] -> [(a, Int)]
+auxFrequencies [] checkedCs lstCOccur = lstCOccur
+auxFrequencies str checkedCs lstCOccur =
+    if elem (head str) checkedCs
+        then auxFrequencies restStr checkedCs lstCOccur
+        else auxFrequencies restStr (checkedCs ++ [fstC]) (lstCOccur ++ [(fstC, (occurrenceInStr fstC str))])
+        where restStr = tail str
+              fstC = head str
 
+occurrenceInStr c str = (length (filter (==c) str))
+
+-- function that sorts a list of tuples based on the value their 2nd value
 sortfrequencies :: Ord a => [(a1, a)] -> [(a1, a)]
 sortfrequencies [] = []
 sortfrequencies tuplesToSort = sortBy (compare `on` snd) tuplesToSort
 
+-- function that sorts a list of LTree based on the frequencies of a Branch/Leaf
 sortTreeList :: [LTree] -> [LTree]
 sortTreeList treeToSort = sortBy (compare `on` getFrequency) treeToSort
 
@@ -59,28 +71,6 @@ valid ((a,b):xs)
 restArgumentList :: [(b, b1)] -> [b]
 restArgumentList list = map fst list
 
--- although there exist another function called "lookup" which kinda does the same thing
-lookup' :: Eq a => [(a, [Char])] -> a -> [Char]
-lookup' [] a = "Value does not exist."
-lookup' l a = 
-    if valid l
-        then lookupAux l a
-        else "Value does not exsist."
-
-lookupAux :: Eq a => [(a, [Char])] -> a -> [Char]      
-lookupAux l a =
-    if fst (head l) == a
-        then snd (head l)
-        else if (length (tail l)) > 0
-            then lookupAux (tail l) a
-            else "Value does not exist."
-
-findfun :: (t, [Char]) -> [Char] -> [Char]
-findfun (a,b) x = 
-    if b == x
-        then b
-        else "Function not found."
-
 --function that merges two LTree types into a new LTree
 treemerge :: LTree -> LTree -> LTree
 treemerge lt1 lt2 = Branch lt1 lt2 treeSymbols treeFrequency
@@ -95,24 +85,29 @@ listToLTree lst =
         then auxListToLTree lst []
         else []
 
+auxListToLTree :: [(Symbol, Frequency)] -> [LTree] -> [LTree]
 auxListToLTree [] lstTree = lstTree
 auxListToLTree lst lstTree =
     auxListToLTree (tail lst) (lstTree ++ [Leaf (fst (head lst)) (snd (head lst))])
 
+-- function for building a Huffman tree based on a list of leaves
 buildHuffmanTree :: [LTree] -> LTree
 buildHuffmanTree [t] = t
 buildHuffmanTree (t1:t2:ts) = buildHuffmanTree (sortTreeList ((treemerge t1 t2):ts))
 
+-- function to encode one character with the help of a tree
+encodeChar :: Symbol -> LTree -> [Char] -> [Char]
 encodeChar c (Leaf s f) encoding = encoding
 encodeChar c (Branch l r syms f) encoding =
     if elem c (getSymbol l)
         then encodeChar c l (encoding ++ "0")
         else encodeChar c r (encoding ++ "1")
 
---decodeStr str tree 
-decodeCharacter [] tree (Leaf s f) decodedStr = (decodedStr ++ [s])
-decodeCharacter enc tree (Leaf s f) decodedStr =  decodeCharacter enc tree tree (decodedStr ++ [s])
-decodeCharacter enc tree (Branch l r syms f) decodedStr =
+-- function to decode an encoded string with the help of a tree
+decodeStr :: [Char] -> LTree -> LTree -> [Symbol] -> [Symbol]
+decodeStr [] tree (Leaf s f) decodedStr = (decodedStr ++ [s])
+decodeStr enc tree (Leaf s f) decodedStr =  decodeStr enc tree tree (decodedStr ++ [s])
+decodeStr enc tree (Branch l r syms f) decodedStr =
     if (head enc) == '0'
-        then decodeCharacter (tail enc) tree l decodedStr
-        else decodeCharacter (tail enc) tree r decodedStr
+        then decodeStr (tail enc) tree l decodedStr
+        else decodeStr (tail enc) tree r decodedStr
